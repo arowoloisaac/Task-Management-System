b38fb4ff8ef2864aec244bcd1bdc6e2cf435c565
arowoloisaac <arowoloisaacabiodun01@gmail.com>
2024-11-24 20:31:23 +0700

    Update git ignore

--- Service/Configuration/DateOnlyJsonConverter.cs
+++ Service/Configuration/DateOnlyJsonConverter.cs
@@ -19,3 +19,4 @@ namespace Project_Manager.Service.Configuration
         }
     }
 }
+ 
--- obj/Debug/net8.0/Project-Manager.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project-Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09138584d05ee92bcb0fe629367d3d3be60bceb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+191e8186afe07405c5dfd1d3b3caae1714905df4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
